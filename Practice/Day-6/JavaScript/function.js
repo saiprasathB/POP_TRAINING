fee4bf1ea2fb25a28614c821f81233949dababab
saiprasathB <saiprasath2805@gmail.com>
2025-02-14 14:47:14 +0530

    Day-6 AN

---
+++ Practice/Day-6/JavaScript/function.js
@@ -0,0 +1,13 @@
+
+function test() {
+  console.log("Hello World!");
+}
+
+test(); // Hello World!
+
+
+ var test2 =() =>{
+    console.log("Hello World!");
+ }
+
+ test2(); // Hello World!   // Arrow function
