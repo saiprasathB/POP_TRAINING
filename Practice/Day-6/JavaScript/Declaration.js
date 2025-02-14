dfe2ab955ad9a1f2ddacba48497a27cbc3031b6b
saiprasathB <saiprasath2805@gmail.com>
2025-02-14 13:16:22 +0530

    Day-6 AN

---
+++ Practice/Day-6/JavaScript/Declaration.js
@@ -0,0 +1,38 @@
+// a=10
+// b="abs"
+// c=true
+// console.log(typeof(a),typeof(b),typeof(c));
+
+// Array=[1,true,"abc"]
+// console.log(Array[0])
+// console.log(Array[1])
+// console.log(Array[2])
+
+// Object={
+//     first :"St.Joseph",
+//     second : "School",
+//     third : "Kolkata",
+//     department : "CSE Department"
+// }
+// console.log(Object)
+
+// Object2={}
+// Object2["first"]="St.Joseph"
+// Object2["second"]="School"
+// Object2["third"]="Kolkata"
+// console.log(Object2)
+
+
+// Object3= new Object()
+// Object3.first="St.Joseph"
+// console.log(Object3)
+
+
+// set =new Set()
+// console.log(set)
+// set.add("Hello")
+// console.log(set)
+// set.add("World")
+// console.log(set)
+
+
