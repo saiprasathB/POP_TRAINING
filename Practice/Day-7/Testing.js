a5aa8e73bfd52a472f3d1d776cff697f67fd1e37
saiprasathB <saiprasath2805@gmail.com>
2025-02-16 21:22:43 +0530

    New Device

---
+++ Practice/Day-7/Testing.js
@@ -0,0 +1,24 @@
+console.log("Logical And Operator");
+console.log(5 && !10)
+console.log(true && 10)
+console.log(false && 10)
+console.log(false && true)
+console.log(true && false)
+
+
+console.log("Logical Or Operator");
+console.log(99 || 99)
+console.log(true || 10)
+console.log(false || 10)
+console.log(false || true)
+console.log("KAJCBNOAC" && "   ")
+
+if ('5'!==5){
+    console.log("True")}
+else
+    {console.log("False")}
+
+
+    console.log(55>>3)
+    console.log(55<<3)
+    "sugge"
