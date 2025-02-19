396d33f36cddd55b98a4fe1def0f73056834e1e5
saiprasathB <saiprasath2805@gmail.com>
2025-02-19 22:10:33 +0530

    Day-9 Task

--- sjitFrontend/src/components/FunctionalComponents/Login.jsx
+++ sjitFrontend/src/components/FunctionalComponents/Login.jsx
@@ -1,8 +1,25 @@
-import { useState } from "react";
+import '../Css/Form.css'
 
 function Login(){
     return(
-        <h1>Login Page</h1>
+      <div> <center><h1>Welcome To my WebSite</h1></center>
+ <center><form action="/about"class="form">
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" placeholder="Enter Username" /></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" placeholder="Enter Password" /></td>
+            </tr>
+            
+        </table>
+        <center> <button>Login</button></center> 
+       <p> Don't have an Account <a href="/signup">Sign Up?</a></p>
+        </form>
+        </center>
+        </div> 
     )
 }
 export default Login
--- sjitFrontend/src/components/FunctionalComponents/SignUp.jsx
+++ sjitFrontend/src/components/FunctionalComponents/SignUp.jsx
@@ -1,6 +1,51 @@
+import'../Css/Signup.css'
 function SignUp(){
     return(
-        <h1>SignUp Page</h1>
+        <div>
+             <center><h1>SignIn</h1></center>
+           <center> <form action="/login" class="formsignup">
+        <table border="0" class="table">
+            <tr>
+                <td><label htmlFor="FirstName">FirstName:</label></td>
+                <td><input type="text" placeholder="Enter FirstName" required /></td>
+            </tr>
+            <br />
+            <tr>
+                <td><label htmlFor="SecondName">SecondName:</label></td>
+                <td><input type="text" placeholder="Enter SecondName" required /></td>
+            </tr>
+            <tr>
+                <td><label htmlFor="Email">Email:</label></td>
+                <td><input type="email" placeholder="Enter Email" required /></td>
+            </tr>
+            <tr>
+                <td><label htmlFor="DOB">DOB:</label></td>
+                <td><input type="date" placeholder="Enter DOB" required /></td>
+            </tr>
+            <tr>
+                <td><label htmlFor="Age">Age:</label></td>
+                <td><input type="number" placeholder="Enter Age" required /></td>
+            </tr>
+            <tr>
+                <td><label htmlFor="Gender">Gender:</label></td>
+                <td> <input type="radio" name="gender" value="Male" />Male </td>
+                <td> <input type="radio" name="gender" value="Female" />Female </td>
+            </tr>
+            <tr>
+                <td><label htmlFor="Password">Password:</label></td>
+                <td><input type="password" placeholder="Enter Password" required /></td>
+            </tr>
+            <tr>
+                <td><label htmlFor="ConPassword">ConfirmPassword:</label></td>
+                <td><input type="password" placeholder="Enter Password" required /></td>
+            </tr>
+            
+        </table>
+        <br />
+      <center> <button>SignIn</button></center>
+    </form>
+    </center>  
+        </div>
     )
 }
 export default SignUp
